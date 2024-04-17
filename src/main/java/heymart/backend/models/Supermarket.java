cc662c4744f529ebc53b8fee68cf608e5a0e233e
ternaksapi <yusufhaikaln7@gmail.com>
2024-04-17 20:08:08 +0700

    [GREEN] Implement Supermarket model

--- src/main/java/heymart/backend/models/Supermarket.java
+++ src/main/java/heymart/backend/models/Supermarket.java
@@ -1,4 +1,30 @@
 package heymart.backend.models;
 
+import java.util.ArrayList;
+import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Supermarket {
+    private Long id;
+    private String name;
+    private User owner;
+    private List<Long> productIds;
+
+    public Supermarket(Long id, String name, User owner, List<Long> productIds) {
+        this.id = id;
+        this.name = name;
+        this.owner = owner;
+        this.productIds = productIds;
+    }
+
+    public void addProductId(Long productId) {
+        productIds.add(productId);
+    }
+
+    public void removeProductId(Long productId) {
+        productIds.remove(productId);
+    }
 }
