0eb79e56fd0faa59eccc3f361de2ace61b9a9124
ternaksapi <yusufhaikaln7@gmail.com>
2024-04-17 20:55:19 +0700

    [GREEN] Implement Builder Design Pattern for Model

--- src/main/java/heymart/backend/models/Supermarket.java
+++ src/main/java/heymart/backend/models/Supermarket.java
@@ -1,5 +1,4 @@
 package heymart.backend.models;
-
 import java.util.ArrayList;
 import java.util.List;
 import lombok.Getter;
@@ -13,18 +12,54 @@ public class Supermarket {
     private User owner;
     private List<Long> productIds;
 
-    public Supermarket(Long id, String name, User owner, List<Long> productIds) {
-        this.id = id;
-        this.name = name;
-        this.owner = owner;
-        this.productIds = productIds;
+    private Supermarket(Builder builder) {
+        this.id = builder.id;
+        this.name = builder.name;
+        this.owner = builder.owner;
+        this.productIds = builder.productIds;
     }
 
     public void addProductId(Long productId) {
+        if (productIds == null) {
+            productIds = new ArrayList<>();
+        }
         productIds.add(productId);
     }
 
     public void removeProductId(Long productId) {
-        productIds.remove(productId);
+        if (productIds != null) {
+            productIds.remove(productId);
+        }
+    }
+
+    public static class Builder {
+        private Long id;
+        private String name;
+        private User owner;
+        private List<Long> productIds;
+
+        public Builder setId(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder setOwner(User owner) {
+            this.owner = owner;
+            return this;
+        }
+
+        public Builder setProductIds(List<Long> productIds) {
+            this.productIds = productIds;
+            return this;
+        }
+
+        public Supermarket build() {
+            return new Supermarket(this);
+        }
     }
 }
