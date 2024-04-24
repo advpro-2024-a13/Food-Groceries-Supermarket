1ebcf2b58cdddc6b3b4c9fa779e84775792fccb1
patrickSevans123 <patricksamuel2107@gmail.com>
2024-04-24 20:36:33 +0700

    [REFACTOR] Implements builder design pattern in product model

--- src/main/java/heymart/backend/models/Product.java
+++ src/main/java/heymart/backend/models/Product.java
@@ -2,6 +2,7 @@ package heymart.backend.models;
 
 import lombok.Getter;
 import lombok.Setter;
+
 import java.util.UUID;
 
 @Getter
@@ -15,4 +16,72 @@ public class Product {
     private String productImagePath;
     private Long productPrice;
     private Long supermarketOwnerId;
+
+    private Product() {}
+
+    public static class Builder {
+        private UUID productId;
+        private String productName;
+        private int productQuantity;
+        private String productCategory;
+        private String productDescription;
+        private String productImagePath;
+        private Long productPrice;
+        private Long supermarketOwnerId;
+
+        public Builder() {}
+
+        public Builder productId(UUID productId) {
+            this.productId = productId;
+            return this;
+        }
+
+        public Builder productName(String productName) {
+            this.productName = productName;
+            return this;
+        }
+
+        public Builder productQuantity(int productQuantity) {
+            this.productQuantity = productQuantity;
+            return this;
+        }
+
+        public Builder productCategory(String productCategory) {
+            this.productCategory = productCategory;
+            return this;
+        }
+
+        public Builder productDescription(String productDescription) {
+            this.productDescription = productDescription;
+            return this;
+        }
+
+        public Builder productImagePath(String productImagePath) {
+            this.productImagePath = productImagePath;
+            return this;
+        }
+
+        public Builder productPrice(Long productPrice) {
+            this.productPrice = productPrice;
+            return this;
+        }
+
+        public Builder supermarketOwnerId(Long supermarketOwnerId) {
+            this.supermarketOwnerId = supermarketOwnerId;
+            return this;
+        }
+
+        public Product build() {
+            Product product = new Product();
+            product.setProductId(productId);
+            product.setProductName(productName);
+            product.setProductQuantity(productQuantity);
+            product.setProductCategory(productCategory);
+            product.setProductDescription(productDescription);
+            product.setProductImagePath(productImagePath);
+            product.setProductPrice(productPrice);
+            product.setSupermarketOwnerId(supermarketOwnerId);
+            return product;
+        }
+    }
 }
