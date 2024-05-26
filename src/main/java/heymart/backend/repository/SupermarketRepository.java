4a3bfa01953db3f297c4dec0d1a21856e06bed70
ternaksapi <yusufhaikaln7@gmail.com>
2024-05-26 23:57:51 +0700

    [FEAT] Added monitoring with Prometheus and Grafana

--- src/main/java/heymart/backend/repository/SupermarketRepository.java
+++ src/main/java/heymart/backend/repository/SupermarketRepository.java
@@ -1,10 +1,7 @@
 package heymart.backend.repository;
 
-import heymart.backend.models.Product;
 import heymart.backend.models.Supermarket;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
 
 public interface SupermarketRepository extends JpaRepository<Supermarket, Long> {
     Supermarket save(Supermarket supermarket);
